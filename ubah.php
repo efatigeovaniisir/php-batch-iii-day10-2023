04042aa0c477cd343e0c7d5e25975c21670b7f97
efati geovani <127381007+efatigeovaniisir@users.noreply.github.com>
2023-08-01 19:57:25 +0900

    add desain ubah.php

--- ubah.php
+++ ubah.php
@@ -20,49 +20,50 @@
 </div>
 
 <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <h1>Ubah Data Makanan</h1>
-            <form action="" method="post">
-                <div class="mb-2">
-                    <label for="nama" class="form-label">Nama Makanan</label>
-                    <Input type="text" class="form-control" id="nama" name="nama" value="PAPEDA"
-                    placeholder="Masukan Nama Makanan" required>
-                </div>
-                <div class="mb-2">
-                    <label for="nama" class="form-label">Harga</label>
-                    <Input type="number" class="form-control" id="harga" name="harga" value=""
-                    placeholder="Masukan Harga" required>
-                </div>
+<a href="index.php" class="btn btn-warning mb-2">kembali</a>
+    <div class="card bg-primary text-light">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Ubah Data Makanan</h1>
+                <form action="" method="post">
+                    <div class="mb-2">
+                        <label for="nama" class="form-label">Nama Makanan</label>
+                        <Input type="text" class="form-control" id="nama" name="nama" value="PAPEDA"
+                            placeholder="Masukan Nama Makanan" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="nama" class="form-label">Harga</label>
+                        <Input type="number" class="form-control" id="harga" name="harga" value=""
+                            placeholder="Masukan Harga" required>
+                    </div>
 
-                <div class="mb-2">
-                    <label for="nama">Warung</label>
-                    <Input id="warung" type="text" name="warung"
-                    placeholder="Masukan Warung" required>
-                </div>
+                    <div class="mb-2">
+                        <label for="nama" class="form-label">Warung</label>
+                        <Input class="form-control" id="warung" type="text" name="warung" placeholder="Masukan Warung" required>
+                    </div>
 
-                <div class="mb-2">
-                    <label for="nama">Alamat Warung</label>
-                    <Input id="alamat" type="text" name="alamat"
-                    placeholder="Masukan Alamat" required>
-                </div>
+                    <div class="mb-2">
+                        <label for="nama" class="form-label">Alamat Warung</label>
+                        <Input class="form-control" id="alamat" type="text" name="alamat" placeholder="Masukan Alamat" required>
+                    </div>
 
-                <div class="mb-2">
-                    <label for="nama">Pemilik Warung</label>
-                    <Input id="pemilik" type="text" name="pemilik_warung"
-                    placeholder="Masukan Pemilik Warung" required>
-                </div>
+                    <div class="mb-2">
+                        <label for="nama" class="form-label">Pemilik Warung</label>
+                        <Input class="form-control" id="pemilik" type="text" name="pemilik_warung" placeholder="Masukan Pemilik Warung"
+                            required>
+                    </div>
 
-                <div class="mb-2">
-                    <button type="submit" name="submit">Simpan</button>
-                </div>
-            </form>
-
-
+                    <div class="mt-2">
+                        <button type="submit" name="submit" class="btn btn-light m-2">Simpan</button>
+                    </div>
+                </form>
 
 
 
 
+
+
+            </div>
         </div>
     </div>
 </div>
